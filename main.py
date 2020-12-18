31a6fafcf8f9dcc0aefcd7b99973664251a17033
Julie Hu <jhu21495@gmail.com>
2020-12-17 16:42:05 -0800

    orange ball moves left and right

--- main.py
+++ main.py
@@ -2,12 +2,15 @@
 import pygame
 """ main loop for Barrys Corner"""
 
+# toy
+
 
 # define a main function
 def main():
 
     # initialize the pygame module
     pygame.init()
+
     # load and set the logo
     logo = pygame.image.load("pawprint.png")
     pygame.display.set_icon(logo)
@@ -16,39 +19,71 @@ def main():
     bg = pygame.image.load('wood.png')
 
     # images
-    mouse = pygame.image.load('mouse.png')
-    mouse = pygame.transform.scale(mouse, (200, 200))
-    orange_ball = pygame.image.load('orange_ball.png')
-    orange_ball = pygame.transform.scale(orange_ball, (300, 300))
-    green_ball = pygame.image.load('green_ball.png')
-    green_ball = pygame.transform.scale(green_ball, (300, 300))
-    pink_ball = pygame.image.load('pink_ball.png')
-    pink_ball = pygame.transform.scale(pink_ball, (300, 300))
-    feather = pygame.image.load('feather.png')
-    feather = pygame.transform.scale(feather, (300, 300))
+    # mouse = pygame.image.load('mouse.png')
+    # mouse = pygame.transform.scale(mouse, (200, 200))
+
+    # green_ball = pygame.image.load('green_ball.png')
+    # green_ball = pygame.transform.scale(green_ball, (300, 300))
+    # pink_ball = pygame.image.load('pink_ball.png')
+    # pink_ball = pygame.transform.scale(pink_ball, (300, 300))
+    # feather = pygame.image.load('feather.png')
+    # feather = pygame.transform.scale(feather, (300, 300))
 
     # create a surface on screen that has the size of 240 x 180
     screen = pygame.display.set_mode((2500, 1300))
 
-    screen.blit(bg, (0, 0))
-    screen.blit(mouse, (50, 50))
-    screen.blit(orange_ball, (10, 50))
-    screen.blit(green_ball, (10, 50))
-    screen.blit(pink_ball, (10, 50))
-    screen.blit(feather, (20, 50))
+    # screen.blit(green_ball, (10, 50))
+    # screen.blit(pink_ball, (10, 50))
+    # screen.blit(feather, (20, 50))
+    pygame.display.set_caption("Barry's Corner")
+
+    # define a variable to control the main loop
+
+    #Toy
+    orange_ball = pygame.image.load('orange_ball.png')
+    orange_ball = pygame.transform.scale(orange_ball, (300, 300))
+    toyX = 1000
+    toyY = 650
+    toy_change = 0
 
     pygame.display.flip()
-    # define a variable to control the main loop
+
+    def toy(x, y):
+        # screen.blit(mouse, (50, 50))
+        screen.blit(orange_ball, (x, y))
+
     running = True
 
     # main loop
     while running:
+
+        screen.blit(bg, (0, 0))
+
         # event handling, gets all event from the event queue
+        # for x in range(100):  #animate 100 frames
+        #     screen.blit(orange_ball, (400, 500))  #erase
+        #     position = position.move(2, 0)  #move player
+        #     screen.blit(orange_ball, (200, 400))  #draw new player
+        #     pygame.display.update()  #and show it all
+        #     pygame.time.delay(100)
+        # toyX += 0.1
         for event in pygame.event.get():
             # only do something if the event is of type QUIT
             if event.type == pygame.QUIT:
                 # change the value to False, to exit the main loop
                 running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    toy_change = -10
+                if event.key == pygame.K_RIGHT:
+                    toy_change = 10
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    toy_change = 0
+
+        toyX += toy_change
+        toy(toyX, toyY)
+        pygame.display.update()
 
 
 # run the main function only if this module is executed as the main script
