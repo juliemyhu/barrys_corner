164225c1e9738509ef8118b4f844c935b8bd8847
Julie Hu <jhu21495@gmail.com>
2021-02-18 11:53:03 -0800

    ball moves sporaticaly

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import pygame
 from random import randint, randrange
 from pygame.time import Clock
+import time
 import pygame.math as math
 """ main loop for Barrys Corner"""
 
@@ -167,14 +168,13 @@ feather = toy('feather.png')
 
 def random_game():
 
+    # initilizing background
     pygame.init()
-
-    # load and set the logo
     logo = pygame.image.load("pawprint.png")
     pygame.display.set_icon(logo)
     pygame.display.set_caption("Barry's Corner")
-
     bg = pygame.image.load('wood.png')
+    screen = pygame.display.set_mode((2500, 1300))
 
     # images
     # mouse = pygame.image.load('mouse.png')
@@ -186,29 +186,34 @@ def random_game():
     # feather = pygame.image.load('feather.png')
     # feather = pygame.transform.scale(feather, (300, 300))
 
-    screen = pygame.display.set_mode((2500, 1300))
-
-    pygame.display.set_caption("Barry's Corner")
-
-    screen.blit(bg, (0, 0))
-    screen.blit(green_ball.image, (100, 50))
+    # screen.blit(green_ball.image, (100, 50))
     # screen.blit(pink_ball, (400, 50))
     # screen.blit(feather, (800, 50))
 
-    pygame.display.flip()
+    # pygame.display.flip()
 
-    def toy(toy_obj):
-        screen.blit(bg, (0, 0))
-        x_offset = randint(0, 2000)
-        y_offset = randint(0, 1000)
-        # x_offset = randrange(-50, 50, 25) + 15
-        # y_offset = randrange(-50, 50, 25) + 15
-        toy_obj.x += x_offset
-        toy_obj.y += y_offset
-        screen.blit(toy_obj.image, (toy_obj.x, toy_obj.y))
+    # def toy(toy_obj):
+    #     screen.blit(bg, (0, 0))
+    #     x_offset = randint(0, 2000)
+    #     y_offset = randint(0, 1000)
+    #     # x_offset = randrange(-50, 50, 25) + 15
+    #     # y_offset = randrange(-50, 50, 25) + 15
+    #     toy_obj.x += x_offset
+    #     toy_obj.y += y_offset
+    #     screen.blit(toy_obj.image, (toy_obj.x, toy_obj.y))
+
+    orange_ball = pygame.image.load('orange_ball.png')
+    orange_ball = pygame.transform.scale(orange_ball, (300, 300))
+    toyX = 1000
+    toyY = 650
+
+    def toy(x, y):
+        screen.blit(orange_ball, (x, y))
 
     running = True
+
     while running:
+
         screen.blit(bg, (0, 0))
 
         for event in pygame.event.get():
@@ -219,8 +224,23 @@ def random_game():
         # toy(green_ball,randomx,0)
         # toy(feather,0,0)
 
-        toy(green_ball)
-        toy(feather)
+        for i in range(10):
+            number = randrange(2000)
+            numberY = randrange(1200)
+        toyX = number
+        toyY = numberY
+
+        if toyX <= -40:
+            toyX = -40
+        elif toyX >= 2225:
+            toyX = 2225
+        if toyY <= -20:
+            toyY = -20
+        elif toyY >= 1070:
+            toyY = 1070
+
+        toy(toyX, toyY)
+        # toy(feather)
 
         # pygame.time.wait(500)
         # toy(feather, x, y)
